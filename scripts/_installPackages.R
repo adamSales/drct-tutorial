cf61ea6b6b7b36eeeb86221aeda27813d03fe47e
Adam Sales <asales@wpi.edu>
2024-07-12 13:02:52 -0500

    separate heterogeneous effects script, other tweaks

--- scripts/_installPackages.R
+++ scripts/_installPackages.R
@@ -1,9 +1,10 @@
 ## run this at the beginning, let it run while we're blabbering on, save time later:
 
 needed.packages <- c("dplyr", "tidyr", "devtools","randomForest","optmatch",
-                     "kableExtra","SuperLearner")
+                     "kableExtra","SuperLearner","sandwich","lmtest",
+                     "ranger","rpart","rpart.plot")
 
 new.packages <- needed.packages[!(needed.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
-devtools::install_github("manncz/dRCT")
+devtools::install_github("manncz/dRCT",dep=TRUE,upgrade=FALSE)
